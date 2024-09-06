a6757a5b154f3bc3bcacbf5f85eb9df949d0f10f
Chandresh Mishra <115541250+Chanduu11@users.noreply.github.com>
2024-09-06 09:29:06 +0530

    array

---
+++ counttoffee.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+ 
+ 
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];   
+    }
+
+    int m;
+    cin >> m;   
+    
+    bubbleSort(arr, n);   
+
+    int count = 0; 
+    
+    for (int i = 0; i < n; i++) {
+        if (m % 5 == 0) {   
+            count++;
+            break;  
+        } else if (m >= arr[i]) {  
+            count++;   
+            m -= arr[i];   
+        } else {
+            break;   
+        }
+    }
+    
+    cout << count << endl;   
+    return 0;
+}
